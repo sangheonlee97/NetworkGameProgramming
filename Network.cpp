64fb6f5a3280295ef0abe00018ab550f6ce960e3
Hyung Ryong Lee <ukrk774@tukorea.ac.kr>
2022-11-08 15:20:55 +0900

    Add pressStart and WaitFiveSecAndStart function

--- Network.cpp
+++ Network.cpp
@@ -1,12 +1,23 @@
 #include "Network.h"
 
+DWORD WINAPI WaitFiveSecAndStart(LPVOID lpParam)
+{
+	WAITING_ROOM* ppNetInfo = (WAITING_ROOM*)lpParam;
+	Sleep(5000);
+	if (ppNetInfo->checkAllReady() == true) {
+		ppNetInfo->sendStart();
+	}
+}
+
 WAITING_ROOM::WAITING_ROOM()
 {
 	for (int i{}; i < 3; ++i) {
 		player[i].nickname[0] = "\0";
 		player[i].isReady = false;
 		player[i].sock = NULL; // CreateThread() 실패한 경우도 NULL
-		player[i].num = 0; // 0 - 플레이어 미식별
+		player[i].num = 0; // 0 - 플레이어 슬롯 비어있음
+
+		sock[i] = (HANDLE)-1; // accept() 실패한 경우도 -1
 	}
 
 	listen = (HANDLE)-1; // listen() 실패한 경우도 -1
@@ -19,4 +30,12 @@ bool WAITING_ROOM::checkReduplication(char* name)
 			return false;
 	}
 	return true;
+}
+
+void WAITING_ROOM::pressStart()
+{
+	if (this->checkAllReady() == true) {
+		HANDLE hthr = CreateThread(NULL, 0, WaitFiveSecAndStart, NULL, 0, NULL);
+		CloseHandle(hthr);
+	}
 }
--- Network.h
+++ Network.h
@@ -12,8 +12,12 @@ class WAITING_ROOM
 public:
 	WAITING_ROOM();
 	bool checkReduplication(char* name);
-
+	bool checkAllReady(); // need checkAllReady
+	void pressStart();
+	void sendStart(); // need sendStart
+	
 private:
 	PlayerInfo player[3]; // need PayerInfo
 	HANDLE listen;
+	HANDLE sock[3]; // need to talk
 };
